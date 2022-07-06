03cff05e99ecfe7035bab6810c137dbae946314f
Angel-luiss <ltucubala@miumg.edu.gt>
2022-07-05 18:36:55 -0600

    Initial Commit

---
+++ hola/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hola
+//
+//  Created by Angel Tucubal on 5/07/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
